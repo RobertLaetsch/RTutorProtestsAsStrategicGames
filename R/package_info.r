9823ae10cfde9b313cf321947f760ab7f976a903
RobertLaetsch <robert.laetsch@uni-ulm.de>
2023-12-27 13:48:50 +0100

    Package

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "ProtestsAsStrategicGames"
+  
+  # Name of problem sets in the package
+  ps = c("Protests-as-Strategic-Games")
+  
+  list(package=package, ps = ps)
+}
